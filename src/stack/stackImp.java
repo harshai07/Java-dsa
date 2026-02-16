7b7aa720b908f2085c5af1d663f464ab6ed72681
Harsha I <P00999850@asianpaints.com>
2026-02-16 11:56:47 +0530

    add the stack implimention

--- src/stack/stackImp.java
+++ src/stack/stackImp.java
@@ -3,6 +3,7 @@ import  java.util.Stack;
 
 public class stackImp {
     public static void main(String[] args) {
+        //in the stack push, pop and peek Time compexcity is O(1)
         Stack<Integer>stack= new Stack<>();
         stack.push(10);
         stack.push(3);
