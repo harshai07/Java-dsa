f503da0e5013e1471cf5744c7016b4f1187bfd34
Harsha.I <115336812+harshai07@users.noreply.github.com>
2026-03-13 01:41:49 +0530

    0975-RANGE-SUM-OF-BST Stats: Time: 0 ms (100%), Space: 53.4 MB (97.9%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.java
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.java
@@ -17,9 +17,14 @@ public class Solution {
     public int rangeSumBST(TreeNode root, int low, int high) {
         if (root == null) return 0;
 
-        int res = (low <= root.val && root.val <= high) ? root.val : 0;
-        res += rangeSumBST(root.left, low, high);
-        res += rangeSumBST(root.right, low, high);
-        return res;
+        if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        }
+        if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        }
+
+        return root.val + rangeSumBST(root.left, low, high) +
+                          rangeSumBST(root.right, low, high);
     }
 }
