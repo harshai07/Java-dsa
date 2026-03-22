ea03a7cd02d4bd63a8d0b7cd4ad47f9e5eecfefa
Harsha.I <115336812+harshai07@users.noreply.github.com>
2026-03-22 19:25:08 +0530

    1014-K-CLOSEST-POINTS-TO-ORIGIN Stats: Time: 51 ms (13.42%), Space: 56.1 MB (35.88%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,7 +1,16 @@
 public class Solution {
-    public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, (a, b) -> (a[0] * a[0] + a[1] * a[1]) -
-                                      (b[0] * b[0] + b[1] * b[1]));
-        return Arrays.copyOfRange(points, 0, k);
+    public int[][] kClosest(int[][] points, int K) {
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparing(a -> a[0]));
+        for (int[] point : points) {
+            int dist = point[0] * point[0] + point[1] * point[1];
+            minHeap.offer(new int[]{dist, point[0], point[1]});
+        }
+
+        int[][] result = new int[K][2];
+        for (int i = 0; i < K; ++i) {
+            int[] point = minHeap.poll();
+            result[i] = new int[]{point[1], point[2]};
+        }
+        return result;
     }
 }
