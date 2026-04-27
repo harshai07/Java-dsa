3a86e6ef89a162f9292bfe8f6c9a0efd7d7a257a
Harsha.I <115336812+harshai07@users.noreply.github.com>
2026-04-27 21:23:52 +0530

    0062-UNIQUE-PATHS Stats: Time: 0 ms (100%), Space: 42.1 MB (65.69%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,16 +1,28 @@
-public class Solution {
-    public int uniquePaths(int m, int n) {
-        int[] row = new int[n];
-        Arrays.fill(row, 1);
+class Solution {
+    private int func(int i , int j , int [][] dp ){
+        //Base
+        if(i==0&& j==0) return 1;
+        //out of bounds 
+        if(i<0||j<0) return 0;
+        //if already visited
+        if(dp[i][j]!=-1) return dp[i][j];
 
-        for (int i = 0; i < m - 1; i++) {
-            int[] newRow = new int[n];
-            Arrays.fill(newRow, 1);
-            for (int j = n - 2; j >= 0; j--) {
-                newRow[j] = newRow[j + 1] + row[j];
-            }
-            row = newRow;
+        //Recursive call from left up and left moves
+        int up = func(i-1,j ,dp);
+        int left = func(i, j-1, dp);
+
+        //store the result return
+        return dp[i][j]=up+left;
+
+    }
+    //funtion to calculate total unique path
+    public int uniquePaths(int m, int n) {
+        //Dp array initalized with -1
+        int [][] dp = new int[m][n];
+        for(int[]row: dp){
+            Arrays.fill(row,-1);
         }
-        return row[0];
+        return func(m-1, n-1, dp);
+        
     }
 }
